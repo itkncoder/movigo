f5a95eea7eb1827c5a2665fb304214740a165987
KNCoder <itkncoder@gmail.com>
2023-02-07 02:41:31 +0500

    .

--- src/components/catalog-card/CatalogCard.jsx
+++ src/components/catalog-card/CatalogCard.jsx
@@ -2,7 +2,7 @@ const CatalogCard = (props) => {
     return (  
         <div className="shadow-gray-900 rounded-md">
             <a href={props.link}>
-                <img src={props.img} alt="movie" className="rounded-md h-80 hover:object-fit duration-300 transition-all object-cover" />
+                <img src={props.img} alt="movie" className="rounded-md h-80 hover:scale-105 duration-300 transition-all object-cover" />
                 <div className="pb-4 flex flex-col items-start pt-1">
                     <h1 className="poppins">lorem opsum dolor sit amet</h1>
                     <p className="text-gray-500">premium</p>
