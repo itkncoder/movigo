66d1a7ef8eb1dfeed34823c05f82c209a3537aa3
KNCoder <itkncoder@gmail.com>
2023-02-14 23:46:44 +0500

    seo

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,7 +9,23 @@ const Home = () => {
     window.scroll(0, 0);
 
     return (  
-        <div>           
+        <div>
+
+            <Helmet>
+                <title>Movigo - Movigo - online kinolar, tarjima qilingan holda va HD sifatda ko'ring</title>
+                <meta name="title" content="Movigo - online kinolar, tarjima qilingan holda va HD sifatda"/>
+                <meta name="description" content="Movigo - online kinolar, tarjima qilingan holda va HD sifatda, yuklab olish imkoniyati va ko'plab kinolar"/>
+                <meta name="keywords" content="movigo, online, kino, movi go, muvigo, muvi, kinoteatr, online kino, filmlar, multfilmlar, anime, yangi kinolar, yangi seriallar, seriallar"/>
+                <meta name="robots" content="index, follow"/>
+                <meta name="description" content="Movigo - online kinolar, tarjima qilingan holda va HD sifatda, yuklab olish imkoniyati va ko'plab kinolar" />
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+                <meta name="language" content="Uzbek"/>
+                <meta name="author" content="Movigo"/>
+                <meta property="og:title" content="Movigo" />
+                <meta property="og:site_name" content="Movigo"/>
+                <meta property="og:description" content="Movigo - online kinolar, tarjima qilingan holda va HD sifatda, yuklab olish imkoniyati va ko'plab kinolar" />
+            </Helmet>
+
             <div className="flex flex-col justify-between">
                 <HomeTop/>
                 <main>
