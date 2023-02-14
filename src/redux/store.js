059234b7619a08a1214ee304f308b0ceacb72866
KNCoder <itkncoder@gmail.com>
2023-02-14 11:49:23 +0500

    maybe works

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,6 @@ import reducer from "./reducer"
 import { configureStore } from "@reduxjs/toolkit"
 
 export const store = configureStore({
-    reducer: reducer,
-    devtools: process.env.NODE_ENV !== "production"
+    reducer,
+    devTools: process.env.NODE_ENV !== "production"
 })
