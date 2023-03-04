3ce5888817bda3d053d5086fb2fabb89c12b41fa
KNCoder <itkncoder@gmail.com>
2023-03-04 18:15:55 +0500

    maybe fixes scroll y

--- src/components/catalog-card/CatalogCard.jsx
+++ src/components/catalog-card/CatalogCard.jsx
@@ -5,7 +5,7 @@ const CatalogCard = ({props, category}) => {
     return (  
         <div className={`shadow-gray-900 rounded-md py-2 w-fit ${category && " mx-3"}`}>
             <Link to={`/watch/${props.title}?id=${props._id}`}>
-                <img src={props.poster} alt="movie" className="rounded-md w-60 h-80 hover:scale-102 transition-all object-cover" />
+                <img src={props.poster} alt="movie" className="rounded-md w-auto sm:w-60 h-80 hover:scale-102 transition-all object-cover" />
                 <div className="pb-4 flex flex-col items-start pt-1">   
                     <h1 className="poppins truncate w-56">{props.title ? props.title : ''}</h1>
                     <p className="text-gray-500 truncate w-52">{props.genres ? props.genres : ''}</p>
