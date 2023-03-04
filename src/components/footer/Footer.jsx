ebdc386a3e243556316da533606696dcb0ebd3af
KNCoder <itkncoder@gmail.com>
2023-03-04 17:07:24 +0500

    fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                 <Logo/>
                 <p className="text-gray-400 text-sm hidden lg:block">©️ 2023, <a href="./">movigo.uz</a>, Barcha huquqlar himoyalangan</p>
                 <div className="flex jusitify-center items-center gap-4">
-                    <a className="hover:text-gray-300" to="https://t.me/nodejs5">Reklama</a>
+                    <a className="hover:text-gray-300" href="https://t.me/nodejs5">Reklama</a>
                 </div>
             </div>
         </footer>
