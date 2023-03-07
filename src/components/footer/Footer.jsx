46f7c24899ff49c8655e00b28eeb1d5034891ffe
KNCoder <itkncoder@gmail.com>
2023-03-07 10:12:44 +0500

    .

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,14 +4,17 @@ import {Link} from "react-router-dom"
 
 const Footer = () => {
     return (  
-        <footer className="flex items-center justify-between mt-10 px-4 bg-[#1E242C]">
-            <div className="max-w-[1200px] mx-auto py-[55px] flex items-center w-full justify-between">
-                <Logo/>
-                <p className="text-gray-400 text-sm hidden lg:block">©️ 2023, <a href="./">movigo.uz</a>, Barcha huquqlar himoyalangan</p>
-                <div className="flex jusitify-center items-center gap-4">
-                    <a className="hover:text-gray-300" target="_blank" href="https://t.me/nodejs5">Reklama</a>
+        <footer className="flex flex-col items-end mt-10 p-4 bg-[#1E242C]">
+            <div className="w-full flex items-center justify-between px-4">
+                <div className="max-w-[1200px] mx-auto py-[40px] flex items-center w-full justify-between">
+                    <Logo/>
+                    <p className="text-gray-400 text-sm hidden lg:block">©️ 2023, <a href="./">movigo.uz</a>, Barcha huquqlar himoyalangan</p>
+                    <div className="flex jusitify-center items-center gap-4">
+                        <a className="hover:text-gray-300" target="_blank" href="https://t.me/nodejs5">Reklama</a>
+                    </div>
                 </div>
             </div>
+            <a className="mb-4" href="https://webmaster.yandex.ru/siteinfo/?site=https://movigo.netlify.app"><img className="rounded-md" width="88" height="31" alt="" border="0" src="https://yandex.ru/cycounter?https://movigo.netlify.app&theme=light&lang=en"/></a>
         </footer>
     );
 }
