2232b39055fad9de837ae072d23ec16d05f7ddb8
KNCoder <itkncoder@gmail.com>
2023-03-09 17:41:23 +0500

    fixed footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,14 +4,14 @@ import {Link} from "react-router-dom"
 
 const Footer = () => {
     return (  
-        <footer className="flex flex-col items-end mt-10 p-4 bg-[#1E242C]">
+        <footer className="flex flex-col items-end mt-10 p-1 bg-[#1E242C]">
             <div className="w-full flex items-center justify-between px-4">
                 <div className="max-w-[1200px] mx-auto py-[40px] flex items-center w-full justify-between">
                     <Logo/>
                     <p className="text-gray-400 text-sm hidden lg:block">©️ 2023, <a href="./">movigo.uz</a>, Barcha huquqlar himoyalangan</p>
                     <div className="flex jusitify-center items-center gap-4">
-                        <a className="text-xs sm:text-sm hover:text-gray-300" target="_blank" href="https://t.me/nodejs5">Texnik xizmatlar</a>
-                        <a className="text-xs sm:text-sm hover:text-gray-300" target="_blank" href="https://t.me/nodejs5">Reklama</a>
+                        <a className="text-xs text-center sm:text-sm hover:text-gray-300" target="_blank" href="https://t.me/nodejs5">Texnik xizmatlar</a>
+                        <a className="text-xs text-center sm:text-sm hover:text-gray-300" target="_blank" href="https://t.me/nodejs5">Reklama</a>
                     </div>
                 </div>
             </div>
